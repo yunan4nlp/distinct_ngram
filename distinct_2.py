dd83701228af8e90f33b2a8e4981e7c602250c88
yunan <yunan@yunans-MacBook-Pro.local>
2019-03-05 21:36:51 +0800

    serious bug

--- distinct_2.py
+++ distinct_2.py
@@ -4,18 +4,21 @@ sp="#####"
 
 def distinct_2(path):
     inFile = open(path, mode="r", encoding="utf8")
-    biword_set = set()
+    bichar_set = set()
     all_bigram_count = 0
     for line in inFile.readlines():
         line = line.strip().split(" ")
-        word_len = len(line)
-        for idx in range(word_len - 1):
-            biword_set.add(line[idx] + sp + line[idx + 1])
-        biword_set.add("<BOS>" + sp + line[0])
-        biword_set.add(line[word_len - 1] + sp + "<EOS>")
-        all_bigram_count += (word_len + 1)
+        sent = ""
+        for word in line:
+            sent += word
+        char_len = len(sent)
+        for idx in range(char_len - 1):
+            bichar_set.add(sent[idx] + sp + sent[idx + 1])
+        bichar_set.add("<BOS>" + sp + sent[0])
+        bichar_set.add(sent[char_len - 1] + sp + "<EOS>")
+        all_bigram_count += (char_len + 1)
 
-    distinct_bigram_count = len(biword_set)
+    distinct_bigram_count = len(bichar_set)
     print("distinct_bigram: ", distinct_bigram_count)
     print("all_bigram: ", all_bigram_count)
     print("distinct 1: " + str(distinct_bigram_count / all_bigram_count))
